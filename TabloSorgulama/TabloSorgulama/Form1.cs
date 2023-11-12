69a587472893118cc6c71946835fd46ed2e2fb8b
Devran ŞEKER <120737148+devranseker@users.noreply.github.com>
2023-11-12 19:15:55 +0300

    TabloSorgulama
    İlişkili Veri Tabanı Hareket Tablo ve Sorgulama Uygulaması.

---
+++ TabloSorgulama/TabloSorgulama/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace TabloSorgulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection sqlbaglantisi = new SqlConnection(@"Data Source=DEVRAN-PC\SQLEXPRESS;Initial Catalog=TabloSorgulama;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Execute TabloSorguProcedur ", sqlbaglantisi);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        // Odev yeni bir satıs işlemini C# formunda yapın (valununmber display nunmber)
+        // ama comboboxlarda secilerrk 
+    }
+}
